53ab300687ab6b763e0486766aca43fd5932e881
Benjamin Bay <benjaminbay@Benjamins-MacBook-Pro.local>
2018-12-01 16:29:03 -0800

    working on alpha-beta pruning

--- chess-agent/src/game.py
+++ chess-agent/src/game.py
@@ -2,12 +2,24 @@
 import chess
 from random import randint
 import sys
+from collections import namedtuple
+
+
+ScoreMove = namedtuple("ScoreMove", ["score","move"])
 
 
 def rand_elem(l):
     size = len(l)
     r = randint(0,size-1)
     return l[r]
+
+
+def max_with_tup(num, tup):
+    return max(num,tup.score)
+
+
+def min_with_tup(num, tup):
+    return min(num,tup.score)
 
 
 class Agent(object):
@@ -95,8 +107,94 @@ class MinMaxAgent(AiAgent):
     def turn(self, board):
         """
         """
-        move = self.recurse_minimax(board, 1)
+        #_, move = self.recurse_minimax2(board, 2, self.color)
+        score, move = self.alpha_beta_max(board, float('-inf'), float('inf'), 2)
         self.do_move(move, board)
+
+
+    def maxi(depth):
+        if depth == 0:
+            return eval_board(board, SCORE_MAP)
+        max_score = float('-inf')
+        for move in board.legal_moves:
+            score = self.mini(board, depth - 1)
+            if score > max_score:
+                max_score = score
+        return max_score
+
+
+    def mini(self, board, depth):
+        if depth == 0:
+            return -eval_board(board, SCORE_MAP)
+        min_score = float('inf');
+        for move in board.legal_moves:
+            score = self.maxi(board, depth - 1)
+            if score < min_score:
+                min_score = score
+        return min_score
+
+
+    def alpha_beta_max(self, board, alpha, beta, depth):
+        if depth == 0:
+            return eval_board(board, SCORE_MAP), board.peek()
+        for move in board.legal_moves:
+            self.do_move(move, board)
+            score, _ = self.alpha_beta_min(board, alpha, beta, depth - 1)
+            board.pop()
+            if score >= beta:
+                return beta     #fail hard beta-cutoff
+            if score > alpha:
+                alpha = score   #alpha acts like max in MiniMax
+                best_move = move
+        return alpha, best_move
+
+
+    def alpha_beta_min(self, board, alpha, beta, depth):
+        if depth == 0:
+            return -1 * eval_board(board, SCORE_MAP), board.peek()
+        for move in board.legal_moves:
+            self.do_move(move, board)
+            score, _ = self.alpha_beta_max(board, alpha, beta, depth - 1)
+            board.pop()
+            if score <= alpha:
+                return alpha     #fail hard alpha-cutoff
+            if score < beta:
+                beta = score     #beta acts like max in MiniMax
+                best_move = move
+        return beta, best_move
+
+
+    def recurse_minimax2(self, board, depth, is_maxer, alpha, beta):
+        if depth == 0:
+            return eval_board(board, SCORE_MAP), board.peek()
+        legal_moves = board.legal_moves
+        best_move = None
+        for i, move in enumerate(legal_moves):
+            self.do_move(move, board)
+            new_score, _ = self.recurse_minimax2(board, depth - 1, not is_maxer)
+            board.pop()
+            if is_maxer:
+#                best_score = -9_999
+                if new_score >= beta:
+                    return beta
+                    best_move = move
+                    best_score = new_score
+                elif new_score == best_score:
+                    r = randint(0,1)
+                    if r == 0:
+                        best_move = move
+                return alpha
+            else:
+#                best_score = 9_999
+                if new_score < best_score:
+                    best_move = move
+                    best_score = new_score
+                elif new_score == best_score:
+                    r = randint(0,1)
+                    if r == 0:
+                        best_move = move
+                return beta
+#        return best_score, best_move
 
 
     def recurse_minimax(self, board, lvl):
@@ -106,10 +204,12 @@ class MinMaxAgent(AiAgent):
         if lvl == self.ply:
             my_moves = []
             my_scores = []
+            # all my possible moves
             for legal_move in legal_moves:
                 self.do_move(legal_move, board)
                 opp_scores = []
                 opp_moves = []
+                # all opponent's possible moves
                 for opp_legal_move in board.legal_moves:
                     self.do_move(opp_legal_move, board)
                     opp_score = eval_board(board, SCORE_MAP)
@@ -129,6 +229,19 @@ class MinMaxAgent(AiAgent):
             for ind in opt_inds:
                 opt_moves.append(my_moves[ind])
             return rand_elem(opt_moves)
+        else:
+            my_moves = []
+            my_scores = []
+            # all my possible moves
+            for legal_move in legal_moves:
+                self.do_move(legal_move, board)
+                opp_scores = []
+                opp_moves = []
+                # all opponent's possible moves
+                for opp_legal_move in board.legal_moves:
+                    self.do_move(opp_legal_move, board)
+                    self.recurse_minimax(board, lvl + 1)
+
 
 
 def eval_board(board, score_map):
@@ -153,15 +266,18 @@ def print_state(board):
     
  
 def play_game(board, p1, p2, console):
+    turn = 0
     while not board.is_game_over():
+        turn += 1
         if console:
-            print("WHITE TURN:")
+            print("TURN {}: WHITE".format(turn))
         p1.turn(board)
         if console:
             print_state(board)
         if not board.is_game_over():
+            turn += 1
             if console:
-                print("BLACK TURN:")
+                print("TURN {}: BLACK".format(turn))
             p2.turn(board)
             if console:
                 print_state(board)
@@ -185,7 +301,8 @@ def play_game(board, p1, p2, console):
 
 def play_rand_ai_game(console=True):
     board = chess.Board()
-    p1 = RandomAiAgent(True)
+#    p1 = RandomAiAgent(True)
+    p1 = MinMaxAgent(True, 1)
     p2 = MinMaxAgent(False, 1)
     play_game(board, p1, p2, console)
     return board
